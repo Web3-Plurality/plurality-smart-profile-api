640f5510019dceb100a74c7c429a2c0d77890104
nehal2210 <nehal.ubit@gmail.com>
2024-11-22 17:35:25 +0500

    fix: add google cresds in env

--- src/services/oauth-service/utils/constants.ts
+++ src/services/oauth-service/utils/constants.ts
@@ -8,7 +8,6 @@ export const FACEBOOK_APP = 'facebook';
 export const FORTNITE_APP = 'fortnite';
 export const GOOGLE_APP = 'google';
 
-
 // erorrs
 export const INTERNAL_SERVER_ERROR = 'Internal Server Error';
 export const TIMEOUT_ERROR = 'Request timeout error in fetching userinfo';
